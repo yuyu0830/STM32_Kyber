cad800a92948f6dfac72128e8dd18b2d9bb28d28
yuyu0830 <yuyu0830@naver.com>
2024-12-09 20:39:34 +0900

    AES Fixing

--- AES_Board/Core/Src/main.c
+++ AES_Board/Core/Src/main.c
@@ -32,14 +32,13 @@ typedef unsigned char uint8_t;
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
-#define TIMEOUT 100
-
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
-#define GetByte() HAL_UART_Receive_IT(&huart2, &byte, 1);
+#define GetData() HAL_UART_Receive_IT(&huart2, &byte, 2);
+#define SendData() HAL_UART_Transmit(&huart2, &result, 1, 100);
 
 /* USER CODE END PM */
 
@@ -48,7 +47,7 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
-uint8_t byte, result;
+uint8_t byte[2], result;
 static const uint8_t sbox[256] = {
     //0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
     0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
@@ -67,6 +66,7 @@ static const uint8_t sbox[256] = {
     0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
     0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
     0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };
+
 
 /* USER CODE END PV */
 
@@ -122,7 +122,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	GetByte();
+	GetData();
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -254,14 +254,15 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
   if (huart->Instance == USART2)
   {
-    HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
     HAL_GPIO_TogglePin (GPIOA, Trigger_Pin);
 
-    result = sbox[byte];
+    result = sbox[byte[0] ^ byte[1]];
 
-	HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
 	HAL_GPIO_TogglePin (GPIOA, Trigger_Pin);
-    }
+
+    SendData();
+
+	//
   }
 }
 
--- AES_PC/AES_DPA/main.c
+++ AES_PC/AES_DPA/main.c
@@ -1,55 +1,129 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <Windows.h>
+#include <time.h>
 
 // Enable both ECB and CBC mode. Note this can be done before including aes.h or at compile-time.
 // E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DECB=1
-#define CBC 1
-#define ECB 1
+#define PORT L"\\\\.\\COM7"
+#define MAX_ROUND 2
+#define SECRET_KEY_REF 0x80
 
-#include "aes.h"
+HANDLE hSerial;
 
-int Init();
+DWORD dwBytesWrite;
+DWORD dwBytesRead;
 
+uint8_t buff[2];
 
-int main(void)
-{
-    return 0;
-}
+int SerialPortConnect();
 
+int main(void) {
+	// 통신 설정
+	HANDLE hSerial;
 
-int Init() {
-	int state;
+	wchar_t PortNo[20] = { 0 }; //contain friendly name
+	swprintf_s(PortNo, 20, PORT);
 
-	// Connect Serial Port 
-	if (state = SerialPortConnect()) {
-		printf("Error code %d : Connect Serial Port Failed..\n", state);
-		// return 1;
-	}
-	else {
-		printf("Log : Connect Serial Port Successful!\n");
-	}
+	hSerial = CreateFile(PortNo, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+	DCB dcbSerialParams = { 0 };	// 통신 설정용 구조체
+	COMMTIMEOUTS timeouts = { 0 };	// 통신 Timeout
 
-	// Open Log File
-	if (state = FileOpen()) {
-		printf("Error code %d : Open File Failed..\n", state);
-		// return 1;
-	}
-	else {
-		//WriteLog(pk, sk);
-		printf("Log : Open File Successful!\n");
+	if (hSerial == INVALID_HANDLE_VALUE) {
+		return 0;
 	}
 
-	// Send Key To Board
-	if (state = SendKey(sk)) {
-		printf("Error code %d : Send Key Failed..\n", state);
-		// return 1;
-	}
-	else {
-		printf("Log : Send Key Successful!\n");
+	// 세팅 구조체
+	dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+
+	if (!GetCommState(hSerial, &dcbSerialParams)) {
+		// Error getting COM port state
+		CloseHandle(hSerial);
+		return 0;
 	}
 
-	printf("Log : Initialize Complete!\n\n");
+	dcbSerialParams.BaudRate = CBR_115200;	// 보드레이트 설정
+	dcbSerialParams.ByteSize = 8;			// 데이터 사이즈 설정
+	dcbSerialParams.StopBits = ONESTOPBIT;	// 스탑비트 설정
+	dcbSerialParams.Parity = NOPARITY;		// 패리티 설정
+
+	if (!SetCommState(hSerial, &dcbSerialParams)) {     // 설정 적용
+		// Error setting COM port state
+		CloseHandle(hSerial);
+		return 0;
+	}
+
+	timeouts.ReadIntervalTimeout = 100;
+	timeouts.ReadTotalTimeoutConstant = 100;
+	timeouts.ReadTotalTimeoutMultiplier = 10;
+
+	timeouts.WriteTotalTimeoutConstant = 100;
+	timeouts.WriteTotalTimeoutMultiplier = 10;
+
+	if (!SetCommTimeouts(hSerial, &timeouts)) {
+		// Error set timeouts
+		CloseHandle(hSerial);
+		return 0;
+	}
+
+	printf("Serial Connect\n");
+
+	
+
+	for (int i = 0; i < 256; i++) {
+		uint8_t plainText[2];
+		
+		//plainText[0] = rand() % 256;
+
+		for (int j = 0; j < 20; j++) {
+			plainText[0] = i;
+			plainText[1] = SECRET_KEY_REF;
+
+			printf("plain text : %02x\n", plainText[0]);
+			if (!WriteFile(hSerial, plainText, 2, &dwBytesWrite, NULL)) {
+				printf("Error : Send Data Failed..\n");
+			}
+			else {
+				printf("Log : %d Data Send Successful!\n", i + 1);
+			}
+
+			if (!ReadFile(hSerial, buff, 1, &dwBytesRead, NULL)) {
+				printf("Error : Receive Data Failed..\n");
+			}
+			else {
+				printf("%02x Data Receive Success!\n", buff[0]);
+			}
+			Sleep(1000);
+		}
+		
+			/*for (int tk = 0; tk < 256; tk++) {
+				plainText[1] = tk;
+				Sleep(10);
+				if (!WriteFile(hSerial, plainText, 2, &dwBytesWrite, NULL)) {
+					printf("Error : Send Data Failed..\n");
+				}
+				else {
+					printf("Log : %d, %d Data Send Successful!\n", i + 1, tk + 1);
+				}
+
+				if (!ReadFile(hSerial, buff, 1, &dwBytesRead, NULL)) {
+					printf("Error : Receive Data Failed..\n");
+				}
+				else {
+					printf("%02x Data Receive Success!\n", buff[0]);
+				}
+				Sleep(5000);
+			
+			}*/
+		
+		printf("\n");
+	}
+
 
 	return 0;
+}
+
+int SerialPortConnect() {
+	
 }
